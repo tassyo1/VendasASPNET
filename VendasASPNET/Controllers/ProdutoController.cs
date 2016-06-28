68098457eb95931343b593c115ac27a64d150ce0
tassyo.santana <tassyo.santana@hotmail.com>
2016-06-28 01:55:53 -0300

    Aula 3 terminada - ProdutoController, index de produto

---
+++ VendasASPNET/Controllers/ProdutoController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using VendasASPNET.DAO;
+using VendasASPNET.Models;
+
+
+namespace VendasASPNET.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        // GET: Produto
+        public ActionResult Index()
+        {
+            Contexto contexto = new Contexto();
+            IList<Produto> produtos = contexto.Produtos.OrderBy(p => p.Nome).ToList();
+            return View(produtos);
+        }
+    }
+}
