1304921af697e4f3222c1188af9da4e937268770
tassyo.santana <tassyo.santana@hotmail.com>
2016-06-28 16:39:18 -0300

    Aula 5 terminada - Validação utilizando ViewBag, ModelSta.AddModelError

--- VendasASPNET/Controllers/ProdutoController.cs
+++ VendasASPNET/Controllers/ProdutoController.cs
@@ -25,12 +25,23 @@ namespace VendasASPNET.Controllers
             Contexto contexto = new Contexto();
             IList<Categoria> categorias = contexto.Categorias.OrderBy(c => c.Nome).ToList();
             contexto.Dispose();
+
+            ViewBag.Produto = new Produto
+            {
+                Categoria = new Categoria()
+            };
             return View(categorias);
         }
 
         
         public ActionResult Adiciona(Produto produto)
         {
+            int idInformatica = 1;
+            if (produto.Categoria.ID.Equals(idInformatica) && produto.Preco < 100)
+            {
+                ModelState.AddModelError("produto.InformaticaComPrecoInvalido", "Produtos da" +
+                    " categoria informática devem custar pelo menos 100 reais");
+            }
             Contexto contexto = new Contexto();
             if (ModelState.IsValid)
             {
@@ -42,6 +53,7 @@ namespace VendasASPNET.Controllers
             }
             else
             {
+                ViewBag.Produto = produto;
                 IList<Categoria>categorias = contexto.Categorias.OrderBy(c => c.Nome).ToList();
                 return View("Form", categorias);
             }
