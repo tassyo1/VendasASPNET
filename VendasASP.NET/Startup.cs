687604b72084a24efcbd39eb1108d75c6eda1a65
tassyo.santana <tassyo.santana@hotmail.com>
2016-06-27 21:00:35 -0300

    Inicial

---
+++ VendasASP.NET/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VendasASP.NET.Startup))]
+namespace VendasASP.NET
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
